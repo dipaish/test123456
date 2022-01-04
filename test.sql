33059473033584821aa93b902fe183052a330b13
Deepak KC <deepak.curator@gmail.com>
2022-01-04 19:18:07 +0200

    Initial commit

---
+++ test.sql
@@ -0,0 +1,6 @@
+# structure of the table
+# when asked to select specific columns, you should only use those 
+# import and export database 
+# example bookinfo
+# classic models
+#select date for re-exam data analysis.
